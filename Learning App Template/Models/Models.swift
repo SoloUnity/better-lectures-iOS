6dc43567adc82a180627db36c7dfb740e032ad62
SoloUnity <GordonAccount@yahoo.com>
2022-06-29 16:20:40 -0400

    Add assets Add data files Add models Parse Json in ContentModel Parse HTML style in ContentModel

---
+++ Learning App Template/Models/Models.swift
@@ -0,0 +1,54 @@
+//
+//  Models.swift
+//  Learning App Template
+//
+//  Created by Gordon Ng on 2022-06-29.
+//
+
+import Foundation
+
+struct Module: Decodable, Identifiable{
+    
+    var id: Int
+    var category: String
+    var content:Content
+    var test: Test
+    
+}
+
+struct Content: Decodable, Identifiable{
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var lessons: [Lesson]
+}
+
+struct Lesson: Decodable, Identifiable{
+    
+    var id: Int
+    var title: String
+    var video: String
+    var duration: String
+    var explanation: String
+    
+}
+
+struct Test: Decodable, Identifiable{
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var questions: [Question]
+}
+
+struct Question: Decodable, Identifiable{
+    
+    var id: Int
+    var content: String
+    var correctIndexx: Int
+    var answers: [String]
+    
+}
--- Learning App Template/ViewModels/ContentModel.swift
+++ Learning App Template/ViewModels/ContentModel.swift
@@ -9,4 +9,50 @@ import Foundation
 
 class ContentModel: ObservableObject{
     
+    @Published var modules = [Module]()
+    var styleData: Data? //Initially nil
+    
+    init(){
+        getLocalData()
+    }
+    
+    func getLocalData(){
+        // Get a url to the json file
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do{
+            // Read the file into a data object
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            // Try to decode json into an array of modules
+            let jsonDecoder = JSONDecoder()
+            
+            // get jsonDecode.decode(type, from) type is what you want obtained from the jsonData you input
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            // Assign parse modules to modules property, updates @Published
+            self.modules = modules
+            
+            
+        }
+        catch{
+            print("Rip JSON doesn't work")
+        }
+        
+        // Parse the style data
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do{
+            // Read the file into a data object
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+        }
+        catch{
+            print("Rip Style gg")
+        }
+        
+       
+        
+    }
 }
