040126b2b502525d11ab15ab43ae168002c4eff5
SoloUnity <GordonAccount@yahoo.com>
2022-06-29 15:35:57 -0400

    Initial Commit

---
+++ Learning App Template/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning App Template
+//
+//  Created by Gordon Ng on 2022-06-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning App Template/Learning_App_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_App_TemplateApp.swift
+//  Learning App Template
+//
+//  Created by Gordon Ng on 2022-06-29.
+//
+
+import SwiftUI
+
+@main
+struct Learning_App_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
